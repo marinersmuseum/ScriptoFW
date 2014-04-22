e8a7272f21f47bdaf010f331bb392de73f96cd4f
adamcarrier <acarrier@marinersmuseum.org>
2014-04-22 15:57:37 -0400

    To do list

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ ScriptoFW
 
 Enables side-by-side (viewer and editor), full-browser-width Scripto transcription in Omeka
 
+To-do list:
+- Make admin theme responsive
+- Improve hiding (via JS)
+
 20140-04-22: Not ready! Do not use!
--- ScriptoFWPlugin.php
+++ ScriptoFWPlugin.php
@@ -7,20 +7,20 @@ class ScriptoFWPlugin extends Omeka_Plugin_AbstractPlugin {
     );
     
     /**
-     * Add css and js in the header of the theme.
+     * Add css and js in the head
      */
     public function hookPublicHead($args) {
       $this->_addHookContent($args, "public_head");
-      queue_css_file('scriptofw');
-      queue_js_file('scriptofw');
+      queue_css_file('scriptofw'); // root > views > public > css
+      queue_js_file('scriptofw'); // root > views > public > javascripts
     }
     public function hookAdminHead($args) {
       $this->_addHookContent($args, "public_head");
-      queue_css_file('scriptofw');
-      queue_js_file('scriptofw');
+      queue_css_file('scriptofw');// root > views > admin > css
+      queue_js_file('scriptofw'); // root > views > admin > javascripts
     }
     private function _addHookContent($args, $hookName)
     {        
-        // Do stuff here.
+        // Do fancy stuff here.
     }
 }
